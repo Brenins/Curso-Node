251c1f195c1f1b71158ce37f2afcddb1dda076da
Breno Ramos <breninsramos@gmail.com>
2022-11-10 20:51:50 -0300

    Adicionando Dados

---
+++ index.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var app = express()
+const port = 3000
+// responde com "hello world" quando uma requisição GET é feita para a homepage (localhost:3000)
+
+// Utilizar Craze para template string com variaveis
+app.get('/', function(req, res) {
+  res.send(`Ouvindo GET na porta ${port}`);
+});
+
+app.listen(port, () => {
+    console.log(`Exemplo ouvindo na porta ${port}`)
+})
+
+app.post("/", (req, res) => {
+    res.send("Teste de Post!!!!")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Exemplo Callback
+ /*
+function minhaFuncao(port, funcao){
+    const minhaString = "Oii"
+
+    funcao(port, minhaString)
+}
+
+function callback(port, minhaString){
+    console.log({port, minhaString})
+}) 
+
+minhaFuncao(3000, callback)
+
+*/
